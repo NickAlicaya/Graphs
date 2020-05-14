b75f50a2fe7670d4160a113730d8b19f37ca2be4
nicanor-alicaya <donalicaya@gmail.com>
2020-05-13 23:37:02 -0400

    started working on social.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,118 @@
+from utils import Queue,Stack,Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    """
+        10
+     /
+    1   2   4  11
+     \ /   / \ /
+      3   5   8
+       \ / \   \
+        6   7   9
+    Write a function that, given the dataset and the ID of an individual in the dataset, returns their earliest known ancestor – the one at the farthest distance from the input individual. If there is more than one ancestor tied for "earliest", return the one with the lowest numeric ID. If the input individual has no parents, the function should return -1.
+    """
+    # UPER
+    # use bft
+    # use len(longest)
+    # if len(longest) == 1 then return -1
+    # check the length of the set of vertices for the longest
+    # if there is no child return -1
+    # while there is an edge:
+    # ancestor[0] is parent ancestor[1] is child         
+  
+        # starting node has no parent then return -1   
+    
+    # initialize with the starting node is equal to the child
+    # after that the child becomes the parent
+    
+
+    # graph = Graph()
+    # # relatives is a node
+    # for relatives in ancestors:
+    #     for relative in relatives:
+    #         graph.add_vertex(relative)
+    #         # print('GRAPHXXXXXX',graph.vertices)  
+
+    # for relatives in ancestors:
+    #     graph.add_edge(relatives[1],relatives[0]) 
+    #     print('GRAPHXXXXXX',graph.vertices)  
+
+
+
+
+
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1],pair[0])
+        print('XXXXXVERTICESXXXX',graph.vertices)
+
+
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        # if path is longer or equal and value is smaller, or path is longer
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)    
+
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor            
+
+
+
+
+
+
+
+
+
+    # graph = Graph()
+    # # relatives is a node
+    # for relatives in ancestors:
+    #     for relative in relatives:
+    #         graph.add_vertex(relative)
+    #         # print('GRAPHXXXXXX',graph.vertices)  
+
+    # for relatives in ancestors:
+    #     graph.add_edge(relatives[1],relatives[0]) 
+    #     print('GRAPHXXXXXX',graph.vertices)        
+
+    # # default path length to compare to 
+    # longest_path = 1
+    # # default earliest ancestor to compare to
+    # earliest_ancestor = -1
+    # # initialize the path with a list containing the starting_node
+    # last_node = 0
+    # ancestors_vert = graph.vertices
+
+    # for v in ancestors_vert:
+
+    #     path = graph.dfs(starting_node, v)
+
+    #     if path is not None and len(path) > longest_path:
+
+    #         last_node = v
+    #     elif not path and longest_path == 1:
+    #         last_node = -1
+
+    # return last_node        
+   
+        
+
+
+
+
+
+
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,5 @@
+import random 
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -28,25 +30,49 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    # def populate_graph(self, num_users, avg_friendships):
+    #     """
+    #     Takes a number of users and an average number of friendships
+    #     as arguments
+
+    #     Creates that number of users and a randomly distributed friendships
+    #     between those users.
+
+    #     The number of users must be greater than the average number of friendships.
+    #     """
+    #     # Reset graph
+    #     self.last_id = 0
+    #     self.users = {}
+    #     self.friendships = {}
+    #     # !!!! IMPLEMENT ME
+
+    #     # Add users
+
+    #     # Create friendships
+
     def populate_graph(self, num_users, avg_friendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
+     # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
-
         # Add users
-
-        # Create friendships
+        for i in range(0, num_users):
+            self.add_user(f"User {i}")
+        # Create Frienships
+        # Generate all possible friendship combinations
+        possible_friendships = []
+        # Avoid duplicates by ensuring the first number is smaller than the second
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        # Shuffle the possible friendships
+        random.shuffle(possible_friendships)
+        # Create friendships for the first X pairs of the list
+        # X is determined by the formula: num_users * avg_friendships // 2
+        # Need to divide by 2 since each add_friendship() creates 2 friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +85,7 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+             
         return visited
 
 
